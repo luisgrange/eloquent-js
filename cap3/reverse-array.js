54a31669062d6f85630562894d4297d2fc3652e6
Luís Fernando Grange Ferreira <luisfernando237grange@outlook.com>
2021-10-28 11:24:16 -0300

    Create reverse-array.js

---
+++ cap3/reverse-array.js
@@ -0,0 +1,11 @@
+let arr = [1,2,3,4,5,6,7,8];
+
+function reverse(array){
+    let newArr = []
+    for(i=array.length; i>=0; i--){
+        newArr.push(array[i]);
+    }
+    return newArr;
+}
+
+console.log(reverse(arr));
